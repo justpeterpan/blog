959d45eb5b1c4418b55270b17db1cfe032865327
justpeterpan <peter.busch@posteo.net>
2024-05-05 00:11:59 +0200

    chore: add cloudflare static page deployment preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"]
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  routeRules: {
+    '/': { prerender: true },
+    '/posts/**': { prerender: true, static: true, swr: true },
+  },
+  nitro: {
+    preset: 'cloudflare-pages-static',
+    prerender: {
+      crawlLinks: true,
+      concurrency: 10,
+      interval: 10,
+    },
+  },
 })
