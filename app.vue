e18ac02219f924d762a02fb8620c9695c051d965
justpeterpan <peter.busch@posteo.net>
2024-05-04 23:52:54 +0200

    refactor: restructure main and post pages

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <NuxtPage />
+    <div class="w-full p-4 bg-black text-white">
+      <ul class="flex flex-row justify-between">
+        <li>
+          <NuxtLink to="/" class="text-white">✦ Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/" class="text-white">✦ Exit</NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <NuxtPage class="m-4" />
   </div>
 </template>
 
