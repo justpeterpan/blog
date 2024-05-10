2a30a944c11a69abad8f72d973bf0238e11a8948
justpeterpan <peter.busch@posteo.net>
2024-05-10 09:31:08 +0200

    feat: add pagination

--- app.vue
+++ app.vue
@@ -6,28 +6,54 @@
       </h1>
     </ContentRenderer>
     <div>
-      <ContentList path="/posts" v-slot="{ list }">
-        <div class="grid gap-2 list-grid mx-1 sm:m-20">
-          <NuxtLink
-            v-for="post in list"
-            :to="getSlug(post._path)"
-            :key="post._path"
-            class="items-stretch bg-amber-200 sm:size-full rounded-2xl flex flex-col-reverse sm:p-4 hover:translate-y-[-2px] duration-100 ease-linear hover:shadow-sm"
+      <div class="grid gap-2 list-grid mx-1 sm:m-20">
+        <NuxtLink
+          v-for="post in posts?.slice(currentIndex, currentIndex + 6)"
+          :to="getSlug(post._path!)"
+          :key="post._path"
+          class="items-stretch bg-amber-200 sm:size-full rounded-2xl flex flex-col-reverse sm:p-4 hover:translate-y-[-2px] duration-100 ease-linear hover:shadow-sm"
+        >
+          <div class="p-4">
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.description }}</p>
+          </div>
+          <div class="flex justify-center">
+            <img
+              :src="post.image.src"
+              alt="blog post cover"
+              class="rounded-[32px] w-20 h-20 sm:w-24 sm:h-24 mx-4 my-8 sm:m-8 img-shadow"
+            />
+          </div>
+        </NuxtLink>
+      </div>
+      <div class="flex gap-2 mx-1 sm:m-20">
+        <button
+          @click="backPagination(posts?.length)"
+          :disabled="currentIndex < 0"
+          :class="
+            currentIndex > 0
+              ? 'cursor-pointer text-neutral-800'
+              : 'cursor-not-allowed text-neutral-400'
+          "
+        >
+          <span v-if="currentIndex > 0">←</span> back
+        </button>
+        {{ currentIndex + 1 }} | {{ posts?.length }}
+        <button
+          @click="nextPagination(posts?.length)"
+          :disabled="!!posts?.length && currentIndex + 6 >= posts?.length"
+          :class="
+            !!posts?.length && currentIndex + 6 >= posts?.length
+              ? 'cursor-not-allowed text-neutral-400'
+              : 'cursor-pointer text-neutral-800'
+          "
+        >
+          next
+          <span v-if="!!posts?.length && currentIndex + 6 <= posts?.length"
+            >→</span
           >
-            <div class="p-4">
-              <h2>{{ post.title }}</h2>
-              <p>{{ post.description }}</p>
-            </div>
-            <div class="flex justify-center">
-              <img
-                :src="post.image.src"
-                alt="blog post cover"
-                class="rounded-[32px] w-20 h-20 sm:w-24 sm:h-24 mx-4 my-8 sm:m-8 img-shadow"
-              />
-            </div>
-          </NuxtLink>
-        </div>
-      </ContentList>
+        </button>
+      </div>
     </div>
     <NuxtPage />
   </div>
@@ -41,12 +67,45 @@ useHead({
   },
 })
 
+const route = useRoute()
+
+const currentIndex = ref(0)
+
+if (route.query.index) {
+  currentIndex.value = Number(route.query.index)
+}
+if (!!route.query.index) {
+  currentIndex.value = 0
+}
+
 function getSlug(path: string) {
   return `/posts/${path.split('/').pop()}`
 }
 
+function nextPagination(length: number | undefined, index: number = 6) {
+  console.log('next')
+  if (length && index < length) {
+    currentIndex.value += index
+  }
+  navigateTo({ query: { index: currentIndex.value } })
+}
+
+function backPagination(length: number | undefined, index: number = 6) {
+  if (currentIndex.value === 0) {
+    currentIndex.value = 0
+    return
+  }
+  if (length && index < length) {
+    currentIndex.value -= index
+    navigateTo({ query: { index: currentIndex.value } })
+  }
+}
+
 const { data } = await useLazyAsyncData('page-data', () =>
   queryContent('/').findOne()
+)
+const { data: posts } = await useLazyAsyncData('posts', () =>
+  queryContent('/posts').find()
 )
 </script>
 
---
+++ content/posts/test-post copy 4.md
@@ -0,0 +1,12 @@
+---
+image:
+  src: '/musicbox.png'
+  alt: 'musicbox'
+  width: 200
+  height: 200
+spotify: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/006sLojE4LF9aHXxlxoqck?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+---
+
+# Hello
+
+First post from nuxt content
